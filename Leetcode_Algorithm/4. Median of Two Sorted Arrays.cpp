8d64c4972ea23a131f9ac33df4a77a8ac1644056
os.kwon <93negaro@gmail.com>
2020-04-02 00:34:07 +0900

    4. Median of Two Sorted Arrays

--- Leetcode_Algorithm/4. Median of Two Sorted Arrays.cpp
+++ Leetcode_Algorithm/4. Median of Two Sorted Arrays.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 double findMedianSortedArrays(vector<int> nums1, vector<int> nums2) {
+	vector<int> v;
+	for (int i = 0; i < nums1.size(); ++i)
+		v.push_back(nums1[i]);
+	for (int i = 0; i < nums2.size(); ++i)
+		v.push_back(nums2[i]);
+
+	sort(v.begin(), v.end());
+	if (v.size() % 2 == 0)
+		return ((double)v[v.size() / 2 - 1] + (double)v[v.size() / 2]) / 2;
+	else
+		return (double)v[v.size()/2];
+
+	/*
+	
 	priority_queue<int> max_heap;
 	priority_queue<int, vector<int>, greater<int>> min_heap;
 
@@ -47,6 +62,8 @@ double findMedianSortedArrays(vector<int> nums1, vector<int> nums2) {
 		else
 			return (double)min_heap.top();
 	}
+
+	*/
 }
 
 int main() {
